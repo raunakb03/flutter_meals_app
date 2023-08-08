37b94dab9b1562e39b429e4653774455179703e4
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-08-08 17:11:03 +0530

    create a favourite meals provider

---
+++ lib/providers/favourites_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
+
+  void toggleMealFavouriteStatus(Meal meal) {
+    final mealIsFavourite = state.contains(meal);
+    if (mealIsFavourite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favouriteMealsProvider =
+    StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>((ref) {
+  return FavouriteMealsNotifier();
+});
