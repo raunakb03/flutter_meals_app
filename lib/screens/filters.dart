9324f46b76f7eada8db8f83dea736bc2ef85759c
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-08-08 18:22:27 +0530

    using riverpod to set active filters and retrieving its value

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -3,10 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 
 class FiltersScreen extends ConsumerStatefulWidget {
-  const FiltersScreen({super.key, required this.currentFiltes});
-
-  final Map<Filter, bool> currentFiltes;
-
+  const FiltersScreen({super.key});
   @override
   ConsumerState<FiltersScreen> createState() {
     return _FiltersScreenState();
@@ -22,10 +19,11 @@ class _FiltersScreenState extends ConsumerState<FiltersScreen> {
   @override
   void initState() {
     super.initState();
-    _glutenFreeFilterSet = widget.currentFiltes[Filter.glutenFree]!;
-    _lactoseFreeFilterSet = widget.currentFiltes[Filter.lactoseFree]!;
-    _vegetarianFilterSet = widget.currentFiltes[Filter.vegetarian]!;
-    _veganFilterSet = widget.currentFiltes[Filter.vegan]!;
+    final activeFilters = ref.read(filtersProvider);
+    _glutenFreeFilterSet = activeFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = activeFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = activeFilters[Filter.vegetarian]!;
+    _veganFilterSet = activeFilters[Filter.vegan]!;
   }
 
   @override
@@ -36,13 +34,13 @@ class _FiltersScreenState extends ConsumerState<FiltersScreen> {
       ),
       body: WillPopScope(
         onWillPop: () async {
-          Navigator.of(context).pop({
+          ref.read(filtersProvider.notifier).setFilters({
             Filter.glutenFree: _glutenFreeFilterSet,
             Filter.lactoseFree: _lactoseFreeFilterSet,
             Filter.vegetarian: _vegetarianFilterSet,
             Filter.vegan: _veganFilterSet,
           });
-          return false;
+          return true;
         },
         child: Column(
           children: [
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -27,13 +27,6 @@ class TabsScreen extends ConsumerStatefulWidget {
 class _TabsScreen extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
 
-  Map<Filter, bool> _selectedFilters = {
-    Filter.glutenFree: false,
-    Filter.lactoseFree: false,
-    Filter.vegetarian: false,
-    Filter.vegan: false,
-  };
-
   void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
@@ -43,17 +36,18 @@ class _TabsScreen extends ConsumerState<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     final meals = ref.watch(mealsProvider);
+    final activeFilters = ref.watch(filtersProvider);
     final availableMeals = meals.where((meal) {
-      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
-      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
         return false;
       }
-      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
         return false;
       }
-      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
         return false;
       }
       return true;
@@ -75,19 +69,14 @@ class _TabsScreen extends ConsumerState<TabsScreen> {
     }
 
     // set the screen to filters screen or categories/favourites screen
-    void _setScreen(String identifier) async {
+    void _setScreen(String identifier) {
       Navigator.of(context).pop();
       if (identifier == 'filters') {
-        final result = await Navigator.of(context).push<Map<Filter, bool>>(
+        Navigator.of(context).push(
           MaterialPageRoute(
-            builder: (ctx) => FiltersScreen(
-              currentFiltes: _selectedFilters,
-            ),
+            builder: (ctx) => const FiltersScreen(),
           ),
         );
-        setState(() {
-          _selectedFilters = result ?? kInitialFilters;
-        });
       }
     }
 
